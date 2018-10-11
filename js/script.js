303adfa7b5cd39669b1a09e9c602cf0a473921ec
Unknown <clement.caillaud97@gmail.com>
2018-10-11 14:16:53 +0200

    Boutons liste et carte

--- js/script.js
+++ js/script.js
@@ -13,3 +13,17 @@ function afficher_carte(div)
     attribution: ' <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
   }).addTo(myMap);
 }
+
+function carte_plein_ecran()
+{
+  $("#resultats").addClass("d-md-none");
+  $("#carte").removeClass("col-xl-4");
+  $("#carte").addClass("col-xl-10 col-md-9 d-md-block");
+}
+
+function affichage_classique()
+{
+  $("#carte").removeClass("col-xl-10 col-md-9 d-md-block");
+  $("#carte").addClass("col-xl-4");
+  $("#resultats").removeClass("d-md-none");
+}
