1c7bb287409178774d8fec1bfbb73d15a7ab397b
Unknown <clement.caillaud97@gmail.com>
2018-10-11 13:32:49 +0200

    Responsive working

---
+++ js/script.js
@@ -0,0 +1,18 @@
+$(function()
+{
+  afficher_carte('carte');
+})
+
+function afficher_carte(div)
+{
+  var myMap = L.map(div).setView([51.505, -0.09], 13);
+
+  L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    maxZoom: 19,
+    attribution: ' <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+  }).addTo(myMap);
+}
+
+
+}
